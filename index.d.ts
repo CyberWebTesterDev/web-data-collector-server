1606e344d52c681896370672eaf0c022012351f6
Anatoly Loskutov <tly-90@yandex.ru>
2021-05-08 23:14:52 +0300

    add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+declare function getMoviesData(
+   x: string,
+   y: string,
+): Promise<
+   {
+      title: string;
+      originalTitle: string;
+      rating: string;
+      filmId: any;
+      year: string;
+      country: string;
+      imgsrc: any;
+      genre: string;
+      description: string;
+      mainActors: string[];
+      director: string;
+   }[]
+>;
