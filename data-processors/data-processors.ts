569e3e2f8ef74021249c392f32b6e9dbd03964c3
Anatoly Loskutov <tly-90@yandex.ru>
2021-05-08 22:59:33 +0300

    refact to ts and clean up

---
+++ data-processors/data-processors.ts
@@ -0,0 +1,92 @@
+interface IProfile {
+   id: string;
+   relation: number;
+   last_seen: { time: number };
+   sex: number;
+   has_photo: number;
+   bdate: string;
+   is_closed: number;
+   can_access_closed: number;
+   followers_count: number;
+   counters: {
+      friends: number;
+      followers: number;
+   };
+}
+
+const counterNotNull = (arr: Array<IProfile>) => {
+   let count = 0;
+   let matches = [];
+
+   for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== undefined && arr[i] !== null) {
+         count++;
+         matches.push(arr[i].id.toString());
+      }
+   }
+
+   return matches;
+};
+
+const processProfileDataFromSearch = (
+   array: Array<IProfile>,
+): IProfile[] | boolean => {
+   return array.map((p) => {
+      if (!p.is_closed && p.can_access_closed) {
+         if (
+            !p.relation ||
+            p.relation == 1 ||
+            p.relation == 6 ||
+            p.relation == 0
+         ) {
+            if (p.last_seen) {
+               const dateSeen = new Date(p.last_seen.time * 1000)
+                  .toISOString()
+                  .split('-');
+               if (
+                  dateSeen[0] === '2021' &&
+                  (dateSeen[1] == '03' || dateSeen[1] == '04')
+               ) {
+                  if (p.counters.friends <= 190 && p.counters.followers < 550) {
+                     console.log(`Profile ${p.id} WAS MATCHED !`);
+                     return p;
+                  }
+                  console.log(
+                     `Profile ${p.id} was not matched by counters.friends(${p.counters.friends}) or counters.followers(${p.counters.followers})`,
+                  );
+                  return;
+               }
+               console.log(
+                  `Profile ${p.id} was not matched by last seen time(${dateSeen})`,
+               );
+               return;
+            }
+
+            console.log(
+               `Profile ${p.id} was not matched by last seen time(${p.last_seen})`,
+            );
+            return;
+         }
+         console.log(
+            `Profile ${p.id} was not matched by RELATION(${p.relation})`,
+         );
+         return;
+      }
+      console.log(
+         `Profile ${p.id} was not matched by ACCESS: is_closed(${p.is_closed}), can_access_closed(${p.can_access_closed})`,
+      );
+      return;
+   });
+};
+
+const waitTimeout = (ms: number): Promise<any> => {
+   return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+   });
+};
+
+module.exports.waitTimeout = waitTimeout;
+
+module.exports.counterNotNull = counterNotNull;
+
+module.exports.processProfileDataFromSearch = processProfileDataFromSearch;
--- data-processors/helper.js
+++ data-processors/helper.js
@@ -1,102 +1,102 @@
 Object.prototype.renameProperty = function (oldName, newName) {
-  // Do nothing if the names are the same
-  if (oldName === newName || !newName) {
-    return this;
-  }
-  // Check for the old property name to avoid a ReferenceError in strict mode.
-  if (this.hasOwnProperty(oldName)) {
-    this[newName] = this[oldName];
-    delete this[oldName];
-  }
-  return this;
+   // Do nothing if the names are the same
+   if (oldName === newName || !newName) {
+      return this;
+   }
+   // Check for the old property name to avoid a ReferenceError in strict mode.
+   if (this.hasOwnProperty(oldName)) {
+      this[newName] = this[oldName];
+      delete this[oldName];
+   }
+   return this;
 };
 
 module.exports.insertFields = [
-  "sex",
-  "vk_id",
-  "first_name",
-  "last_name",
-  "counters_followers",
-  "counters_friends",
-  "counters_photos",
-  "last_seen",
-  "city",
-  "is_closed",
+   'sex',
+   'vk_id',
+   'first_name',
+   'last_name',
+   'counters_followers',
+   'counters_friends',
+   'counters_photos',
+   'last_seen',
+   'city',
+   'is_closed',
 ];
 
 module.exports.profileMapper = (profile) => {
-  console.log(`profileMapper enter`);
-  console.log(profile);
+   console.log(`profileMapper enter`);
+   console.log(profile);
 
-  profile["last_seen.time"]
-    ? (profile["last_seen.time"] = new Date(
-        profile["last_seen.time"].replace(" ", "T").replace(" ", "Z")
-      ))
-    : "Unknown";
+   profile['last_seen.time']
+      ? (profile['last_seen.time'] = new Date(
+           profile['last_seen.time'].replace(' ', 'T').replace(' ', 'Z'),
+        ))
+      : 'Unknown';
 
-  const mappingTable = {
-    "city.title": "city",
-    "country.title": "country",
-    "counters.friends": "counters_friends",
-    "counters.followers": "counters_followers",
-    "counters.photos": "counters_photos",
-    bdate: "birth_date",
-    sex: "sex",
-    first_name: "first_name",
-    last_name: "last_name",
-    id: "vk_id",
-    "last_seen.time": "last_seen",
-    is_closed: "is_closed",
-  };
+   const mappingTable = {
+      'city.title': 'city',
+      'country.title': 'country',
+      'counters.friends': 'counters_friends',
+      'counters.followers': 'counters_followers',
+      'counters.photos': 'counters_photos',
+      bdate: 'birth_date',
+      sex: 'sex',
+      first_name: 'first_name',
+      last_name: 'last_name',
+      id: 'vk_id',
+      'last_seen.time': 'last_seen',
+      is_closed: 'is_closed',
+   };
 
-  for (let key in profile) {
-    profile.renameProperty(key, mappingTable[key]);
-    profile.deactivated = false;
-  }
+   for (let key in profile) {
+      profile.renameProperty(key, mappingTable[key]);
+      profile.deactivated = false;
+   }
 
-  console.log(`profileMapper result`);
-  console.log(profile);
+   console.log(`profileMapper result`);
+   console.log(profile);
 
-  return profile;
+   return profile;
 
-  //console.log(`Renamed object is:`);
-  //console.log(profile);
+   //console.log(`Renamed object is:`);
+   //console.log(profile);
 };
 
 module.exports.profileMapperTs = (profile) => {
-  //console.log(`profileMapper enter`);
-  //console.log(profile);
-  if (profile.deactivated) {
-    return { vk_id: profile.id, deactivated: true };
-  }
+   //console.log(`profileMapper enter`);
+   //console.log(profile);
+   if (profile.deactivated) {
+      return { vk_id: profile.id, deactivated: true };
+   }
 
-  profile["last_seen.time"] = new Date(profile["last_seen.time"] * 1000);
+   profile['last_seen.time'] = new Date(profile['last_seen.time'] * 1000);
 
-  const mappingTable = {
-    "city.title": "city",
-    "country.title": "country",
-    "counters.friends": "counters_friends",
-    "counters.followers": "counters_followers",
-    "counters.photos": "counters_photos",
-    bdate: "birth_date",
-    sex: "sex",
-    first_name: "first_name",
-    last_name: "last_name",
-    id: "vk_id",
-    "last_seen.time": "last_seen",
-    is_closed: "is_closed",
-  };
+   const mappingTable = {
+      'city.title': 'city',
+      'country.title': 'country',
+      'counters.friends': 'counters_friends',
+      'counters.followers': 'counters_followers',
+      'counters.photos': 'counters_photos',
+      bdate: 'birth_date',
+      sex: 'sex',
+      first_name: 'first_name',
+      last_name: 'last_name',
+      id: 'vk_id',
+      'last_seen.time': 'last_seen',
+      is_closed: 'is_closed',
+   };
 
-  for (let key in profile) {
-    profile.renameProperty(key, mappingTable[key]);
-    profile.deactivated = false;
-  }
+   for (let key in profile) {
+      profile.renameProperty(key, mappingTable[key]);
+      profile.deactivated = false;
+   }
 
-  //console.log(`profileMapper result`);
-  //console.log(profile);
+   //console.log(`profileMapper result`);
+   //console.log(profile);
 
-  return profile;
+   return profile;
 
-  //console.log(`Renamed object is:`);
-  //console.log(profile);
+   //console.log(`Renamed object is:`);
+   //console.log(profile);
 };
